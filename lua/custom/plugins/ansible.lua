3ead5e497061051507e312c54dc94443c7efa87a
Whipplash <mattsnoopy2@gmail.com>
2023-10-27 12:25:20 -0400

    Ansible's back - though there's linting issues when not editing in dir directly.

---
+++ lua/custom/plugins/ansible.lua
@@ -0,0 +1,29 @@
+local util = require 'lspconfig.util'
+return {
+  require'lspconfig'.ansiblels.setup{
+  cmd = { 'ansible-language-server', '--stdio' },
+  settings = {
+    ansible = {
+      python = {
+        interpreterPath = 'python',
+      },
+      ansible = {
+        path = 'ansible',
+      },
+      executionEnvironment = {
+        enabled = false,
+      },
+      validation = {
+        enabled = true,
+        lint = {
+          enabled = true,
+          path = 'ansible-lint',
+        },
+      },
+    },
+  },
+  filetypes = { 'yml','yaml','yaml.ansible' },
+  root_dir = util.root_pattern('ansible.cfg', '.ansible-lint'),
+  single_file_support = false
+  }
+}
