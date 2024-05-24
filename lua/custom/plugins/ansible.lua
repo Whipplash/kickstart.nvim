d961342b01ce1a6069dae8d7f30881acf50a0aa5
Matthew Kenkel <104021958+mk33650@users.noreply.github.com>
2024-05-24 10:04:10 -0400

    Ansible and conform

--- lua/custom/plugins/ansible.lua
+++ lua/custom/plugins/ansible.lua
@@ -1,29 +1,29 @@
 local util = require 'lspconfig.util'
 return {
-  require'lspconfig'.ansiblels.setup{
-  cmd = { 'ansible-language-server', '--stdio' },
-  settings = {
-    ansible = {
-      python = {
-        interpreterPath = 'python',
-      },
+  require 'lspconfig'.ansiblels.setup {
+    cmd = { 'ansible-language-server', '--stdio' },
+    settings = {
       ansible = {
-        path = 'ansible',
-      },
-      executionEnvironment = {
-        enabled = false,
-      },
-      validation = {
-        enabled = true,
-        lint = {
+        python = {
+          interpreterPath = 'python',
+        },
+        ansible = {
+          path = 'ansible',
+        },
+        executionEnvironment = {
+          enabled = false,
+        },
+        validation = {
           enabled = true,
-          path = 'ansible-lint',
+          lint = {
+            enabled = true,
+            path = 'ansible-lint',
+          },
         },
       },
     },
-  },
-  filetypes = { 'yml','yaml','yaml.ansible' },
-  root_dir = util.root_pattern('ansible.cfg', '.ansible-lint'),
-  single_file_support = false
+    filetypes = { 'yml', 'yaml', 'yaml.ansible' },
+    root_dir = util.root_pattern('ansible.cfg', '.ansible-lint'),
+    single_file_support = false
   }
 }
